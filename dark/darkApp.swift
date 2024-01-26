74d083d3384895b70e7be1696f825dc36ded02fc
MORI <105579842+hibiki333155555@users.noreply.github.com>
2024-01-26 22:21:02 +0900

    Initial Commit

---
+++ dark/darkApp.swift
@@ -0,0 +1,17 @@
+//
+//  darkApp.swift
+//  dark
+//
+//  Created by 森響輝 on 2024/01/26.
+//
+
+import SwiftUI
+
+@main
+struct darkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
